969c11efea6b2be67ed1c2b11b367b5d7a4faf65
MrTibbz <12767925+MrTibbz@users.noreply.github.com>
2026-01-26 03:40:38 +0000

    Bugs Fixes and Shroom Update

--- Teleport_Menu_v2/Il2Cpp/TeleportMenu/TeleportMenu.cs
+++ Teleport_Menu_v2/Il2Cpp/TeleportMenu/TeleportMenu.cs
@@ -14,7 +14,7 @@ using Il2CppScheduleOne.Quests;
 
 
 
-[assembly: MelonInfo(typeof(ScheduleITeleportMenu.Main), "Teleport Menu", "2.1.0", "MrTibbz")]
+[assembly: MelonInfo(typeof(ScheduleITeleportMenu.Main), "Teleport Menu", "2.2.0", "MrTibbz")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace ScheduleITeleportMenu
@@ -141,7 +141,7 @@ namespace ScheduleITeleportMenu
                 var loc = allLocations.FirstOrDefault(l => l.Name == fav.Name && l.Category == fav.Category);
                 if (loc != null) loc.IsFavorite = true;
             }
-            MelonLogger.Msg("Initialization complete.");
+            MelonLogger.Msg("[Teleport Menu] Initialization complete.");
         }
 
         public override void OnUpdate()
@@ -201,22 +201,25 @@ namespace ScheduleITeleportMenu
         #region UI
         public override void OnGUI()
         {
-            // Handle right-click for Back navigation
-            if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+            if (isMenuOpen)
             {
-                if (currentMenu == MenuState.Favorites ||
-                    currentMenu == MenuState.Settings ||
-                    currentMenu == MenuState.SubMenu)
+                // Handle right-click for Back navigation
+                if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
                 {
-                    currentMenu = MenuState.Main;
-                    scrollPosition = Vector2.zero; // reset scrollbar
-                    Event.current.Use(); // consume so it doesn’t trigger button clicks
-                }
-                else if (currentMenu == MenuState.Main)
-                {
-                    // close menu if on main
-                    //isMenuOpen = false;
-                   // Event.current.Use();
+                    if (currentMenu == MenuState.Favorites ||
+                        currentMenu == MenuState.Settings ||
+                        currentMenu == MenuState.SubMenu)
+                    {
+                        currentMenu = MenuState.Main;
+                        scrollPosition = Vector2.zero; // reset scrollbar
+                        Event.current.Use(); // consume so it doesn’t trigger button clicks
+                    }
+                    else if (currentMenu == MenuState.Main)
+                    {
+                        // close menu if on main
+                        //isMenuOpen = false;
+                        // Event.current.Use();
+                    }
                 }
             }
 
@@ -584,6 +587,7 @@ namespace ScheduleITeleportMenu
             allLocations.Add(new TeleportLocation { Name = "Construction site", Position = new SerializableVector3(new Vector3(-130f, -3f, 97f)), Category = "World" });
             allLocations.Add(new TeleportLocation { Name = "Water Front", Position = new SerializableVector3(new Vector3(51.5f, 1f, 95f)), Category = "World" });
             allLocations.Add(new TeleportLocation { Name = "Westville", Position = new SerializableVector3(new Vector3(-137.2f, -3f, 44f)), Category = "World" });
+            allLocations.Add(new TeleportLocation { Name = "Outside Sewer Office", Position = new SerializableVector3(new Vector3(48f, -8f, 66f)), Category = "World" });
             // -------------------- Dead Drops --------------------
             allLocations.Add(new TeleportLocation { Name = "North Arcade Wall", Position = new SerializableVector3(new Vector3(-48f, -3f, 148f)), Category = "Dead Drops" });
             allLocations.Add(new TeleportLocation { Name = "Behind Thompson Construction", Position = new SerializableVector3(new Vector3(-36f, 1f, 113f)), Category = "Dead Drops" });
@@ -613,6 +617,11 @@ namespace ScheduleITeleportMenu
             allLocations.Add(new TeleportLocation { Name = "Albert Hoover's Stash", Position = new SerializableVector3(new Vector3(-18f, -3f, 147f)), Category = "Supplier Stashes" });
             allLocations.Add(new TeleportLocation { Name = "Shirley Watt's Stash", Position = new SerializableVector3(new Vector3(-66f, -1.5f, 32f)), Category = "Supplier Stashes" });
             allLocations.Add(new TeleportLocation { Name = "Salvador Moreno's Stash", Position = new SerializableVector3(new Vector3(148f, 1f, 35f)), Category = "Supplier Stashes" });
+            allLocations.Add(new TeleportLocation { Name = "Fungul Phil's Stash", Position = new SerializableVector3(new Vector3(13.1f, 1f, 0.4f)), Category = "Supplier Stashes" });
+            // -------------------- Sewers  --------------------
+            //allLocations.Add(new TeleportLocation { Name = "Outside Sewer Office", Position = new SerializableVector3(new Vector3(48f, -8f, 66f)), Category = "Sewers" });
+            //allLocations.Add(new TeleportLocation { Name = "Storage Unit Sewer Door", Position = new SerializableVector3(new Vector3(-8.4f, -3f, 115.5f)), Category = "Sewers" });
+            //allLocations.Add(new TeleportLocation { Name = "Salvador Moreno's Stash", Position = new SerializableVector3(new Vector3(148f, 1f, 35f)), Category = "Sewers" });
             // -------------------- Saved Locations --------------------
             foreach (var saved in savedLocations)
             {
